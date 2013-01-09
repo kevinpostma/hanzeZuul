8bfb51c4374ab467e62acd9e9790188f6a388301
Kevin Postma <ke.postma@st.hanze.nl>
2013-01-09 10:06:30 +0100

    First zuul project

---
+++ school-zuul/src/main/java/com/hanze/zuul/App.java
@@ -0,0 +1,13 @@
+package com.hanze.zuul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
