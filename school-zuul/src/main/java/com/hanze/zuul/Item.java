397ecb5c7b219f507ad750f8a041a2d7bf7b0d48
Kevin Postma <ke.postma@st.hanze.nl>
2013-01-10 18:24:39 +0100

    klasse player en Item toegevoegd

---
+++ school-zuul/src/main/java/com/hanze/zuul/Item.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hanze.zuul;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Item {
+    
+    private String itemName;
+    private int itemPickupPoints;
+    
+    
+}
---
+++ school-zuul/src/main/java/com/hanze/zuul/Player.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hanze.zuul;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Player {
+    private String name;
+    private int score;
+    private List<Item> itemList = new ArrayList<Item>();
+    
+    
+}
